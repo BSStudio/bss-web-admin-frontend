b49385d9d6b6a53a2b783eb70c18334681e122fe
Bence Csik <22349790+csikb@users.noreply.github.com>
2022-11-22 08:36:54 +0000

    fix tests

--- src/app/features/event/components/event-table/event-table.component.spec.ts
+++ src/app/features/event/components/event-table/event-table.component.spec.ts
@@ -1,11 +1,18 @@
 import { EventTableComponent } from './event-table.component';
-import { MockBuilder, MockRender } from 'ng-mocks';
+import { MockBuilder, MockInstance, MockRender, ngMocks } from 'ng-mocks';
 import { EventModule } from '../../event.module';
+import { EventService } from '../../services/event.service';
+import { EMPTY } from 'rxjs';
 
 describe('EventTableComponent', () => {
   beforeEach(() => MockBuilder(EventTableComponent, EventModule));
 
   it('should create', () => {
+    MockInstance(EventService, (instance) =>
+      ngMocks.stub(instance, {
+        getEvents: () => EMPTY,
+      })
+    );
     MockRender(EventTableComponent);
   });
 });
