a4e7de4864e11c7ce415a8ccfc877ddcfec46ab7
Bence Csik <22349790+csikb@users.noreply.github.com>
2023-01-17 17:50:21 +0000

    test

--- src/app/shared/components/toast-content-routed/toast-content-routed.component.ts
+++ src/app/shared/components/toast-content-routed/toast-content-routed.component.ts
@@ -2,11 +2,11 @@ import { Component, Input } from '@angular/core'
 import { ToastContent } from 'carbon-components-angular'
 
 @Component({
-  selector: 'app-toast-content-routed',
+  selector: 'app-toast-content-routed[toast]',
   template: `
     <h3 ibmToastTitle>{{ toast.title }}</h3>
     <div ibmToastSubtitle>
-      <span ibmToastSubtitle>{{ toast.subtitle }}</span>
+      <span>{{ toast.subtitle }}</span>
       <a ibmLink *ngFor="let link of toast.links" [routerLink]="link.href">{{ link.text }}</a>
     </div>
     <p ibmToastCaption>{{ toast.caption }}</p>
