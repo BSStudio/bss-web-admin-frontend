909e45a4f0139833dc2435f2de5db4ca879afc21
bcsik <bcsik@cisco.com>
2024-02-21 12:43:15 +0000

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: '',
+      children: [
+        {
+          path: '',
+        },
+        {
+          path: 'video',
+          loadComponent: () => import('./features/video/video.component').then(c => c.VideoComponent)
+        },
+        {
+          path: 'event',
+          loadComponent: () => import('./features/event/event.component').then(c => c.EventComponent)
+        },
+        {
+          path: 'member',
+          loadComponent: () => import('./features/member/member.component').then(c => c.MemberComponent)
+        }
+      ],
+    }
+];
